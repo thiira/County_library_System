8e570e406d21dbfd06c73e94755795d9571e5ed3
thiira <97959190+thiira@users.noreply.github.com>
2022-02-21 14:46:04 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+/* Project: County Library System
+Author: Titus Njiiru
+Date: Oct, 2021
+Copiler: GNU GCC
+Language: C99
+License MIT
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+printf("\tCounty Library\n");
+printf("Welcome Mr.Titus. \n");
+printf("What would you like to do?");
+printf("1. Add new User\n");
+printf("2.Add Book. \n");
+printf("selected action");
+return 0;
+}
