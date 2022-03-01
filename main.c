a57ac1b6022b67b3d880972a8c62d0ddc314776c
thiira <97959190+thiira@users.noreply.github.com>
2022-03-01 17:22:25 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,22 +1,49 @@
-/* Project: County Library System
-Author: Titus Njiiru
-Date: Oct, 2021
-Copiler: GNU GCC
-Language: C99
-License MIT
-
+/*
+County library management system
+By Mr Titus
+FEB 2022
+MIT license
+C89 compiler
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-
-printf("\tCounty Library\n");
-printf("Welcome Mr.Titus. \n");
-printf("What would you like to do?");
-printf("1. Add new User\n");
-printf("2.Add Book. \n");
-printf("selected action");
-return 0;
+int menu() {
+    int action;
+    printf("Select new action below\n");
+    printf("1. Add new Patron\n");
+    printf("2.View all Patrons\n");
+    printf("3. View all books\n");
+    printf("4. Add new Book\n");
+    printf("Your action:\n");
+    scanf("%d",&action);
+    if(action< 1 || action > 4){
+        printf("Invalid Action. Try again\n");
+    }
+    return action;
+}
+void execute_action(int action) {
+switch(action){
+    case 1:
+    printf("adding a new patron\n");
+    break;
+    case 2:
+    printf("list of all patrons\n");
+    break;
+    case 3:
+    printf("list of all books\n");
+    break;
+    case 4:
+    printf("adding a new book\n");
+    break;
+    default: printf("Invalid action.\n");
+    }
+}
+int main()
+{
+    printf("COUNTY LIBRARY SYSTEM\n");
+    printf("Welcome Mr. Titus\n");
+    execute_action(menu());
+    return 0;
 }
